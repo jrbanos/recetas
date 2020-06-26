2aa28113f193f0eda55139d3ef90ab68d7ff803b
Jose Ramon Baños Atalaya <50395211+jrbanos@users.noreply.github.com>
2020-06-26 20:17:26 +0200

    a

--- utils/generar_usuarios.js
+++ utils/generar_usuarios.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Usuario = require(__dirname + '/../models/usuario.js');
 var CryptoJS = require("crypto-js");
 
-mongoose.connect('mongodb://recetas.jbatalaya.site:27017/recetas');
+mongoose.connect('mongodb://jbatalaya.site:27017/recetas');
 
 Usuario.collection.drop();
 let usu1 = new Usuario({
